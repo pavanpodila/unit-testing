68b7a56a2ec51582b0e34551b3dc4cf0e813f23e
Pavan Podila <pavan.podila@gmail.com>
2016-09-13 21:57:13 -0400

    adding simple tests
    (cherry picked from commit ed54fa3)

---
+++ src/simple.spec.js
@@ -0,0 +1,61 @@
+describe('Hello World', () => {
+
+    let obj;
+
+    beforeEach(() => {
+        obj = {
+            greeting: 'hello world',
+            greet(name){
+                return `hello ${name}`
+            },
+
+            lazyGreet(name, callback, delay=500) {
+                setTimeout(()=>{
+                    callback(this.greet(name));
+                }, delay);
+            }
+        };
+    });
+
+
+    it('should have a greeting', () => {
+        expect(obj.greeting).toEqual('hello world');
+    });
+
+    it('should greet the person', () => {
+        const greeting = obj.greet('fannie');
+
+        expect(greeting).toMatch('hello fannie');
+    });
+
+
+    it('should greet lazily default duration', (done) => {
+        obj.lazyGreet('fannie', (greeting)=>{
+            expect(greeting).toMatch('hello fannie');
+            done();
+        });
+
+    });
+
+    describe('when greeting lazily, using jasmine.clock()', () => {
+
+        beforeEach(() => {
+            jasmine.clock().install();
+        });
+
+        afterEach(() => {
+            jasmine.clock().uninstall();
+        });
+
+        it('should greet after longer durations', () => {
+            obj.lazyGreet('fannie', (greeting)=>{
+                expect(greeting).toMatch('hello fannie');
+            }, 10000);
+
+            jasmine.clock().tick(10000);
+        });
+
+    });
+
+
+});
